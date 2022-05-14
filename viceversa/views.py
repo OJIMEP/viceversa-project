f117d8cc1094f68e013ee598eb1ec58becfcc715
vasily <post_lv@mail.ru>
2022-05-14 16:47:18 +0300

    reverse +

--- viceversa/views.py
+++ viceversa/views.py
@@ -9,6 +9,8 @@ def home(request: django.http.HttpRequest):
 def reverse(request: django.http.HttpRequest):
     print(type(request.GET))
     user_text = request.GET['user-text']
+    words_count = len(user_text.split())
     print(user_text)
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'user_text': user_text, 'reversed_text': reversed_text})
+    return render(request, 'reverse.html',
+                  {'user_text': user_text, 'reversed_text': reversed_text, 'words_count': words_count})
