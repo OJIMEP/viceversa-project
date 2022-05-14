a00ff1f32a92b4431d6cae39b23e20dbcca54305
vasily <post_lv@mail.ru>
2022-05-14 16:42:27 +0300

    reverse +

--- viceversa/views.py
+++ viceversa/views.py
@@ -1,9 +1,14 @@
+import django.http
 from django.shortcuts import render
 
 
-def home(request):
+def home(request: django.http.HttpRequest):
     return render(request, 'home.html')
 
 
-def reverse(request):
-    return render(request, 'reverse.html')
+def reverse(request: django.http.HttpRequest):
+    print(type(request.GET))
+    user_text = request.GET['user-text']
+    print(user_text)
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {'user_text': user_text, 'reversed_text': reversed_text})
